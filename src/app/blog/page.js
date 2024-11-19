140785e8b75b90ac3c533280927b1aaad8684dfd
douglasjch <douglasjheuer@hotmail.com>
2024-11-19 09:01:18 -0800

    reval 2

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -4,7 +4,7 @@ async function getData() {
    const domain = getDomain()
    const endpoint = `${domain}/api/posts` // -> third party api request??
     // const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
-    const res = await fetch(endpoint, {next: {revalidate: 10} }) // HTTP GET
+    const res = await fetch(endpoint, {revalidate: 10 }) // HTTP GET
 // cache: 'no-store'
     if (!res.ok) {
         throw new Error("Failed to fetch data")
