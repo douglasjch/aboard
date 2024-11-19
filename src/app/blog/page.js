46babfef6ec745f43f6e07a193bf2764e5e8cd96
douglasjch <douglasjheuer@hotmail.com>
2024-11-18 19:07:53 -0800

    update domain

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,20 +1,19 @@
-
+import getDomain from "@app/lib/getDomain"
 async function getData() {
     // 1 endpoint - API?
-   // const domain = getDomain()
-   // const endpoint = "http://localhost:3000/api/posts"
-   // const endpoint = `${domain}/api/posts` // -> third party api request??
+   const domain = getDomain()
+   const endpoint = `${domain}/api/posts` // -> third party api request??
     // const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
-    // const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
+    const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
 
-    // if (!res.ok) {
-    //     throw new Error("Failed to fetch data")
-    // }
+    if (!res.ok) {
+        throw new Error("Failed to fetch data")
+    }
 
     // if (res.headers.get("content-type") !== "application/json") {
-        return {items: []}
+    //    return {items: []}
     // }
-    // return res.json()
+     return res.json()
 }
 
 export default async function BlogPage() {
