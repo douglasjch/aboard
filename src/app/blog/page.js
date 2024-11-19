08188d4a163008501c1d3a1aaf690768eacf83ba
douglasjch <douglasjheuer@hotmail.com>
2024-11-19 09:09:34 -0800

    absent cache

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -4,8 +4,8 @@ async function getData() {
    const domain = getDomain()
    const endpoint = `${domain}/api/posts` // -> third party api request??
     // const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
-    const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
-// cache: 'no-store'
+    const res = await fetch(endpoint) // HTTP GET
+// cache: 'no-store'  , {cache: 'no-store' }
     if (!res.ok) {
         throw new Error("Failed to fetch data")
     }
