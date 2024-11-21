b2f2a2b1fcdbc1ac1e0cc9ed20c4eed27780cdb3
douglasjch <douglasjheuer@hotmail.com>
2024-11-21 09:52:37 -0800

    swr added

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,14 +1,17 @@
 import getDomain from "@/app/lib/getDomain"
-import BlogCard from "./card"
+import BlogCard from './card'
+// import { helloWorld } from "@/app/lib/db"
+
+
 async function getData() {
-    // 1 endpoint - API?
-   const domain = getDomain()
-   const endpoint = `${domain}/api/posts` // -> third party api request??
- //    const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
- //    const res = await fetch(endpoint, {method: 'GET'}) // HTTP GET
- //    const res = await fetch(endpoint, {cache: 'force-cache' }) // HTTP GET
-   const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
-    
+            // 1 endpoint - API?
+    const domain = getDomain()
+    const endpoint = `${domain}/api/posts` // -> third party api request??
+            // const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET     
+            //    const res = await fetch(endpoint, {method: 'GET'}) // HTTP GET
+            //    const res = await fetch(endpoint, {cache: 'force-cache' }) // HTTP GET
+    const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
+
     if (!res.ok) {
         throw new Error("Failed to fetch data")
     }
@@ -20,17 +23,19 @@ async function getData() {
 }
 
 
-
 export default async function BlogPage() {
-
-  const data = await getData()
-  const items = data && data.items ? [...data.items] : []
-
-  return <main>
-      <h1>Posts:</h1>
-      <h1></h1>
-  {items && items.map((item, idx) => {
-      return <BlogCard title={item.title} key={`post-${idx}`} />
-  }) }
-  </main>
+    const data = await getData()
+   // const dbHello = await helloWorld()
+    const items = data && data.items ? [...data.items] : []
+    return <main>
+        <h1>Hello World</h1>
+        {/* <p>DB Response: {JSON.stringify(dbHello)}</p> */}
+        <p>Posts:</p>
+        {items && items.map((item, idx)=>{
+            return <BlogCard title={item.title} key={`post-${idx}`} />
+        })}
+    </main>
 }
+
+export const runtime = 'edge' // nodejs
+export const preferredRegion = "iad1"
