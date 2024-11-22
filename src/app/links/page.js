007413b026fee870dd8db976f333311a198dce33
douglasjch <douglasjheuer@hotmail.com>
2024-11-22 10:41:18 -0800

    abc added

---
+++ src/app/links/page.js
@@ -0,0 +1,11 @@
+import LinksCreateForm from './createForm';
+
+// import LinksHTMLTable from './table'
+export default function LinksPage() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      
+      <LinksCreateForm />
+    </main>
+  )
+}
